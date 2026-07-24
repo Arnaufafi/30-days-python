3889160f80ae70372f784815172944f3116573cf
Arnaufafi <arnau.fabregas@estudiants.urv.cat>
2026-07-24 14:47:49 +0200

    Add examples for map, filter, and reduce functions in Day 14 of 30DaysOfPython Challenge

--- day_14/higher_order_functions.py
+++ day_14/higher_order_functions.py
@@ -91,3 +91,68 @@
 
 # Define a call function before map, filter or reduce, see examples.
 
+from functools import reduce
+
+
+def square(x):
+    return x*x
+
+result = map(square, [1, 2, 3])
+print(list(result))
+
+def is_even(x):
+    return x % 2 == 0
+
+result = filter(is_even, [1,2,3,4])
+print(list(result))
+
+def add(x, y):
+    return x + y
+
+result = reduce(add, [1, 2, 3, 4])
+print(result)
+
+# Use map to create a new list by changing each country to uppercase in the countries list
+countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+
+result = map(lambda country: country.upper(), countries)
+print(list(result))
+
+# Use map to create a new list by changing each number to its square in the numbers list
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+result = map(square, numbers)
+print(list(result))
+
+# Use map to change each name to uppercase in the names list
+names = names = ['Asabeneh', 'Lidiya', 'Ermias', 'Abraham']
+
+result = map(lambda name : name.upper(), names)
+
+# Use filter to filter out countries containing 'land'.
+result = filter(lambda country: "land" in country, countries )
+print(list(result))
+
+# Use filter to filter out countries having exactly six characters.
+result = filter(lambda country: len(country) == 6, countries)
+print(list(result))
+
+# Use filter to filter out countries containing six letters and more in the country list.
+result = filter(lambda country: len(country) >= 6, countries)
+print(list(result))
+
+# Use filter to filter out countries starting with an 'E
+result = filter(lambda country: country[0] == "E", countries)
+
+# Chain two or more list iterators (eg. arr.map(callback).filter(callback).reduce(callback))
+result = map(lambda country: country.upper(),filter(lambda country: len(country) == 6, countries))
+print(list(result))
+
+# Declare a function called get_string_lists which takes a list as a parameter and then returns a list containing only string items.
+def get_string_list(input_list):
+    return list(filter(lambda item: isinstance(item, str), input_list))
+
+# Use reduce to sum all the numbers in the numbers list.
+result = reduce(lambda x, y: x+y, numbers)
+print(result)
+
